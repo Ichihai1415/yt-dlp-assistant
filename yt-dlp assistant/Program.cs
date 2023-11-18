2d46779b91a0a0ccf8b7bd7538273f489cb8042e
Ichihai1415 <projects3141.gif@gmail.com>
2023-11-18 20:58:47 +0900

    プロジェクト ファイルを追加します。

---
+++ yt-dlp assistant/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yt_dlp_assistant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
