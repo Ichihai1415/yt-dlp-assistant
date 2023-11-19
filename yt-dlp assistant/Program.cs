9962a5bdbfa8207091e8ac3afc7b89d022abbd52
Ichihai1415 <projects3141.gif@gmail.com>
2023-11-19 11:18:22 +0900

    調整　色付け

--- yt-dlp assistant/Program.cs
+++ yt-dlp assistant/Program.cs
@@ -8,6 +8,7 @@ namespace yt_dlp_assistant
     {
         static void Main(string[] args)
         {
+            ConsoleColor defaultColor = Console.ForegroundColor;
             Console.WriteLine("yt-dlp.exeとffmpeg.exeを準備して同じフォルダに配置してください。");
             Console.WriteLine("yt-dlp.exe:https://github.com/yt-dlp/yt-dlp/releases/latest");
             Console.WriteLine("ffmpeg.exe:https://ffmpeg.org/download.html (わからない場合調べてください)");
@@ -25,7 +26,9 @@ namespace yt_dlp_assistant
                         else
                         {
                             Console.WriteLine("yt-dlp.exeのパスを入力してください。");
+                            Console.ForegroundColor = ConsoleColor.Yellow;
                             ytdlpPath_ = Console.ReadLine().Replace("\"", "");
+                            Console.ForegroundColor = defaultColor;
                         }
                         if (File.Exists(ytdlpPath_) && ytdlpPath_.Contains("yt-dlp.exe"))
                         {
@@ -49,7 +52,9 @@ namespace yt_dlp_assistant
                     while (pro == -1)
                     {
                         Console.WriteLine("処理を選択してください。　0:直接入力 1:コマンド保存 2:保存済みコマンド表示 3:シンプルダウンロード");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                         string _pro = Console.ReadLine();
+                        Console.ForegroundColor = defaultColor;
                         if (_pro == "0")
                             pro = 0;
                         else if (_pro == "1")
@@ -63,13 +68,29 @@ namespace yt_dlp_assistant
                     {
                         case 0:
                             Console.WriteLine("コマンドを入力してください。");
-                            Process process = Process.Start(Console.ReadLine());
-                            process.WaitForExit();
+
+                            Process process1 = new Process();
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            process1.StartInfo.FileName = Console.ReadLine();
+                            Console.ForegroundColor = defaultColor;
+
+                            //process1.StartInfo.Arguments = $"-o \"{path}\\%(title)s.mp4\" {url} -f mp4";
+                            process1.StartInfo.UseShellExecute = false;
+                            process1.StartInfo.RedirectStandardOutput = true;
+                            process1.Start();
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            while (!process1.StandardOutput.EndOfStream)
+                                Console.WriteLine(process1.StandardOutput.ReadLine());
+                            process1.WaitForExit();
+                            process1.Dispose();
+                            Console.ForegroundColor = defaultColor;
                             Console.WriteLine("完了しました。");
                             break;
                         case 1:
                             Console.WriteLine("保存するコマンドやメモを一行入力してください。`settings\\commands.txt`に追加されます。");
+                            Console.ForegroundColor = ConsoleColor.DarkBlue;
                             string command = Console.ReadLine();
+                            Console.ForegroundColor = defaultColor;
                             if (File.Exists("settings\\commands.txt"))
                                 command = (File.ReadAllText("settings\\commands.txt") + "\n" + command).Replace("\n\n", "\n");
                             File.WriteAllText("settings\\commands.txt", command);
@@ -77,21 +98,26 @@ namespace yt_dlp_assistant
                         case 2:
                             if (File.Exists("settings\\commands.txt"))
                             {
-                                Console.WriteLine("----------");
+                                Console.ForegroundColor = ConsoleColor.Blue;
                                 Console.WriteLine(File.ReadAllText("settings\\commands.txt"));
-                                Console.WriteLine("----------");
+                                Console.ForegroundColor = defaultColor;
                             }
                             else
-                                Console.WriteLine("`settings\\commands.txt`が見つかりません。先に保存してください。");
+                                Console.WriteLine("`settings\\commands.txt`が見つかりませんでした。先に保存してください。");
                             break;
                         case 3:
                             Console.WriteLine("`{yt-dlp.exeのパス} -o \"{入力されたフォルダのパス}\\%(title)s.mp4\" {入力されたURL} -f mp4`を実行します。スキップする場合URLを空白にしてください。");
                             Console.WriteLine("保存するフォルダのパスを入力してください。(空白だとyt-dlpのフォルダ、`/c/`でこのyt-dlp assistant.exeがあるフォルダ、`/c/\\output`のように)");
+                            Console.ForegroundColor = ConsoleColor.Yellow;
                             string path = Console.ReadLine().Replace("\"", "").Replace("/c/", Path.GetFullPath("yt-dlp assistant.exe").Replace("yt-dlp assistant.exe", ""));
+                            Console.ForegroundColor = defaultColor;
                             Console.WriteLine("URLを入力してください。");
+                            Console.ForegroundColor = ConsoleColor.Yellow;
                             string url = Console.ReadLine();
+                            Console.ForegroundColor = defaultColor;
                             if (url != "")
                             {
+                                Console.ForegroundColor = ConsoleColor.Cyan;
                                 Console.WriteLine($"{ytdlpPath} -o \"{path}\\%(title)s.mp4\" {url} -f mp4");
                                 Process process2 = new Process();
                                 process2.StartInfo.FileName = ytdlpPath;
@@ -100,12 +126,12 @@ namespace yt_dlp_assistant
                                 process2.StartInfo.RedirectStandardOutput = true;
                                 process2.Start();
 
-                                //Console.WriteLine(process2.StandardOutput.ReadToEnd());
+                                Console.ForegroundColor = ConsoleColor.Green;
                                 while (!process2.StandardOutput.EndOfStream)
-                                {
                                     Console.WriteLine(process2.StandardOutput.ReadLine());
-                                }
                                 process2.WaitForExit();
+                                process2.Dispose();
+                                Console.ForegroundColor = defaultColor;
                                 Console.WriteLine("完了しました。");
                             }
                             break;
@@ -115,7 +141,7 @@ namespace yt_dlp_assistant
                 catch (Exception e)
                 {
                     Console.WriteLine(e.ToString());
-                }//todo:cmdでこれ開けば色とか経過とかいい感じにできないかな？
+                }
         }
     }
 }
